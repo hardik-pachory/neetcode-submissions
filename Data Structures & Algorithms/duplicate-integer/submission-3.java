0010d4950940ac00664c1266a4e8be0057e445b6
Hardik Pachory <84628216+hardik-pachory@users.noreply.github.com>
2026-07-27 18:59:08 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> setData = new HashSet<>();
+        for(int num: nums){
+            if(setData.contains(num)){
+                return true;
+            }
+            else {
+                setData.add(num);
+            }
+        }
+        return false;
+    }
+}
